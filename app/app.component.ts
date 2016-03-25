5010ef4220d218c16d0a683b221a446900b4f49d
Kyle <kderrick1988@gmail.com>
2016-03-25 07:41:10 -0700

    add auto instantiated foods and selected food function

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,16 +4,26 @@ import { Component } from 'angular2/core';
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>Food Item Tracker App!</h1>
+      <h3 *ngFor="#food of foods" (click)="foodWasSelected(food)">{{ food.name }}</h3>
     <div>
   `
 })
 export class AppComponent {
-
+  public foods: Food[];
+  constructor() {
+    this.foods = [
+      new Food("Pizza", "Pizza is healthy!", 5000, 0),
+      new Food("Meatball SandWich", "They were the size of baseballs!", 2000, 1),
+      new Food("Oatmeal", "Made it vegan!", 250, 2),
+      new Food("Banana", "Overly ripe", 100, 3)
+    ];
+  }
+  foodWasSelected(clickedFood: Food): void {
+    console.log(clickedFood);
 }
 
 export class Food {
   constructor(public name: string, public details: string, public calories: number, public id: number) {
-
   }
 }
